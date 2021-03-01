50cd24c52b75a78ad6c14026071027447b1b2873
Simon Cropp <simon.cropp@gmail.com>
2021-03-01 16:54:19 +1100

    Update Result.cs

--- src/Replicant/Result.cs
+++ src/Replicant/Result.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using Replicant;
 
 [DebuggerDisplay("ContentPath={ContentPath}, MetaPath={MetaPath}, Status={Status}")]
-internal readonly struct Result
+readonly struct Result
 {
     public string ContentPath { get; }
     public string MetaPath { get; }
