cabade62048811e0076dd34bd7548b4d743b7f8d
Simon Cropp <simon.cropp@gmail.com>
2021-03-06 13:31:39 +1100

    Update MatrixTests.cs

--- src/Tests/MatrixTests.cs
+++ src/Tests/MatrixTests.cs
@@ -13,13 +13,15 @@ using Xunit;
 public class MatrixTests
 {
     static VerifySettings sharedSettings;
+    static DateTimeOffset now = new(2000, 1, 1, 0, 0, 0, TimeSpan.Zero);
+    static DateTimeOffset inPast = now.AddDays(-1);
+    static DateTimeOffset inFuture = now.AddDays(1);
 
     static MatrixTests()
     {
         sharedSettings = new VerifySettings();
         sharedSettings.UseDirectory("../MatrixResults");
     }
-
 
     public static IEnumerable<object[]> DataForIntegration()
     {
@@ -143,161 +145,6 @@ public class MatrixTests
     {
         return $"StatusForMessage_{response}_staleIfError={staleIfError}";
     }
-
-    //[Theory]
-    //[MemberData(nameof(GetData))]
-    //internal async Task ExistingFile(HttpResponseMessage response, Timestamp fromDisk)
-    //{
-    //    var fileName = BuildFileName(response, fromDisk);
-    //    try
-    //    {
-    //        await Verifier.Verify("foo")
-    //            .UseDirectory("../MatrixResults")
-    //            .UseFileName(fileName);
-    //    }
-    //    finally
-    //    {
-    //        response.Dispose();
-    //    }
-    //}
-
-    //static string BuildFileName(HttpResponseMessage response, Timestamp fromDisk)
-    //{
-    //    var builder = new StringBuilder();
-    //    var cacheControl = response.Headers.CacheControl;
-    //    if (cacheControl != null)
-    //    {
-    //        builder.Append($"wCache={cacheControl.ToString().Replace(", ", ",")}_");
-    //    }
-
-    //    var webExpires = response.Content.Headers.Expires;
-    //    if (webExpires == null)
-    //    {
-    //        builder.Append("wExpires=null_");
-    //    }
-    //    else
-    //    {
-    //        builder.Append($"wExpires={webExpires.Value:yyyyMMdd}_");
-    //    }
-
-    //    var webMod = response.Content.Headers.LastModified;
-    //    if (webMod == null)
-    //    {
-    //        builder.Append("wMod=null_");
-    //    }
-    //    else
-    //    {
-    //        builder.Append($"wMod={webMod.Value:yyyyMMdd}_");
-    //    }
-
-    //    var webEtag = response.Headers.ETag;
-    //    if (webEtag == null)
-    //    {
-    //        builder.Append("wMod=null_");
-    //    }
-    //    else
-    //    {
-    //        var etag = Etag.FromResponse(response);
-    //        if (etag.IsEmpty)
-    //        {
-    //            builder.Append("wMod=null_");
-    //        }
-    //        else
-    //        {
-    //            builder.Append($"wMod={etag.ForFile.Substring(1)}_");
-    //        }
-    //    }
-
-    //    if (fromDisk.Expiry == null)
-    //    {
-    //        builder.Append("dExpiry=null_");
-    //    }
-    //    else
-    //    {
-    //        builder.Append($"dExpiry={fromDisk.Expiry.Value:yyyyMMdd}_");
-    //    }
-
-    //    builder.Append($"dMod={fromDisk.Modified:yyyyMMdd}_");
-
-    //    if (fromDisk.Etag.IsEmpty)
-    //    {
-    //        builder.Append("dEtag=null_");
-    //    }
-    //    else
-    //    {
-    //        builder.Append($"dEtag={fromDisk.Etag.ForFile.Substring(1)}_");
-    //    }
-
-    //    return builder.ToString().Trim('_');
-    //}
-
-    ////https://web.dev/http-cache/
-
-    //public static IEnumerable<object[]> GetData()
-    //{
-    //    DateTimeOffset now = new(2000, 1, 1, 0, 0, 0, TimeSpan.Zero);
-    //    var inPast = now.AddDays(-1);
-    //    var inFuture = now.AddDays(1);
-    //    var uri = "foo";
-    //    var hash = Hash.Compute(uri);
-    //    foreach (var fileExpiry in new DateTimeOffset?[] {null, now, inPast, inFuture})
-    //    foreach (var fileModified in new[] {now, inPast})
-    //    foreach (var fileEtag in etags)
-    //    {
-    //        Timestamp timestamp = new(fileExpiry, fileModified, fileEtag, hash);
-    //        HttpResponseMessage response = new(HttpStatusCode.BadRequest);
-    //        yield return new object[]
-    //        {
-    //            response,
-    //            timestamp
-    //        };
-    //    }
-    //    foreach (var webEtag in etags)
-    //    foreach (var cacheControl in cacheControls)
-    //    foreach (var fileExpiry in new DateTimeOffset?[] {null, now, inPast, inFuture})
-    //    foreach (var fileModified in new[] {now, inPast})
-    //    foreach (var fileEtag in etags)
-    //    {
-    //        Timestamp timestamp = new(fileExpiry, fileModified, fileEtag, hash);
-    //        HttpResponseMessage response = new(HttpStatusCode.NotModified);
-    //        //TODO: can LastModified or Expires for a NotModified?
-    //        //response.Content.Headers.LastModified = webMod;
-    //        //response.Content.Headers.Expires = webExpiry;
-    //        if (!webEtag.IsEmpty)
-    //        {
-    //            response.Headers.TryAddWithoutValidation("ETag", webEtag.ForWeb);
-    //        }
-    //        response.Headers.CacheControl = cacheControl;
-    //        yield return new object[]
-    //        {
-    //            response,
-    //            timestamp
-    //        };
-    //    }
-    //    foreach (var webExpiry in new DateTimeOffset?[] {null, inFuture})
-    //    foreach (var webMod in new DateTimeOffset?[] {null, now, inPast})
-    //    foreach (var webEtag in etags)
-    //    foreach (var cacheControl in cacheControls)
-    //    foreach (var fileExpiry in new DateTimeOffset?[] {null, now, inPast, inFuture})
-    //    foreach (var fileModified in new[] {now, inPast})
-    //    foreach (var fileEtag in etags)
-    //    {
-    //        Timestamp timestamp = new(fileExpiry, fileModified, fileEtag, hash);
-    //        HttpResponseMessage response = new(HttpStatusCode.OK);
-    //        response.Content.Headers.LastModified = webMod;
-    //        response.Content.Headers.Expires = webExpiry;
-    //        if (!webEtag.IsEmpty)
-    //        {
-    //            response.Headers.TryAddWithoutValidation("ETag", webEtag.ForWeb);
-    //        }
-    //        response.Headers.CacheControl = cacheControl;
-    //        yield return new object[]
-    //        {
-    //            response,
-    //            timestamp
-    //        };
-    //    }
-    //}
 
     static Etag[] etags = new Etag[]
     {
