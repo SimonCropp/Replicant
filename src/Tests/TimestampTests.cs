cbc816f6cc375a9020c80bbf986fa15193512de8
Simon Cropp <simon.cropp@gmail.com>
2021-03-23 15:26:39 +1100

    more tests

--- src/Tests/TimestampTests.cs
+++ src/Tests/TimestampTests.cs
@@ -9,17 +9,19 @@ public class TimestampTests
     [Fact]
     public Task FromPath()
     {
-        var timestamps = new Dictionary<string, Timestamp>();
+        var timestamps = new List<Timestamp>();
         foreach (var file in new[]
         {
             "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_S{etag}.bin",
+            "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_Setag.bin",
             "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_W{etag}.bin",
+            "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_Wetag.bin",
             "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_.bin",
         })
         {
-            timestamps.Add(file, Timestamp.FromPath(file));
+            timestamps.Add(Timestamp.FromPath(file));
         }
 
-        return Verifier.Verify(timestamps.Values);
+        return Verifier.Verify(timestamps);
     }
 }
