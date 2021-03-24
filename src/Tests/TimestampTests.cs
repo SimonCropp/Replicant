c4294bd4d7c6aa1d5248ec47bd482fd9dcade089
Simon Cropp <simon.cropp@gmail.com>
2021-03-24 19:47:05 +1100

    Update TimestampTests.cs

--- src/Tests/TimestampTests.cs
+++ src/Tests/TimestampTests.cs
@@ -12,11 +12,11 @@ public class TimestampTests
         var timestamps = new List<Timestamp>();
         foreach (var file in new[]
         {
-            "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_S{etag}.bin",
-            "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_Setag.bin",
-            "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_W{etag}.bin",
-            "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_Wetag.bin",
-            "C:\\Dir\\92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_.bin",
+            "/Dir/92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_S{etag}.bin",
+            "/Dir/92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_Setag.bin",
+            "/Dir/92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_W{etag}.bin",
+            "/Dir/92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_Wetag.bin",
+            "/Dir/92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_.bin",
         })
         {
             timestamps.Add(Timestamp.FromPath(file));
