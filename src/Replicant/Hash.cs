4881ef1c708e1a939ffafe8833f782eb1c2dd72e
Simon Cropp <simon.cropp@gmail.com>
2025-05-04 22:49:07 +1000

    better perf via ToHexStringLower

--- src/Replicant/Hash.cs
+++ src/Replicant/Hash.cs
@@ -4,12 +4,12 @@ static class Hash
 {
     public static string Compute(string value)
     {
-        #if NET7_0_OR_GREATER
+#if NET7_0_OR_GREATER
         var hash = SHA1.HashData(Encoding.UTF8.GetBytes(value));
-        #else
+#else
         using var sha = SHA1.Create();
         var hash = sha.ComputeHash(Encoding.UTF8.GetBytes(value));
-        #endif
-        return string.Concat(hash.Select(b => b.ToString("x2")));
+#endif
+        return ConvertPolyfill.ToHexStringLower(hash);
     }
 }
