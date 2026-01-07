8c166d163899a9a5a4643e64a6fe2c2b2160686f
Simon Cropp <simon.cropp@gmail.com>
2026-01-07 21:07:05 +1100

    Purge should only process bin files (#314)

--- src/Tests/FilePairTests.cs
+++ src/Tests/FilePairTests.cs
@@ -2,6 +2,19 @@
 public class FilePairTests
 {
     [Test]
+    public void FromContentFile_WithJsonFile_CreatesSelfReferencingPair()
+    {
+        // FromContentFile expects a .bin content file and derives the .json meta path.
+        // If given a .json file, both Content and Meta will point to the same file.
+        var jsonPath = "/cache/hash_2021-03-22T095023_Setag.json";
+
+        var pair = FilePair.FromContentFile(jsonPath);
+
+        AreEqual(jsonPath, pair.Content);
+        AreEqual(jsonPath, pair.Meta);
+    }
+
+    [Test]
     public void SetExpiry_ShouldSetMinFileDate_WhenExpiryIsNull()
     {
         var path = Path.GetTempFileName();
