5df9f5f66f4772acec90e50bddb410715e12a0b4
Simon Cropp <simon.cropp@gmail.com>
2026-01-07 22:21:30 +1100

    Better memory usage in Timestamp.FromPath (#318)

--- src/Replicant/GlobalUsings.cs
+++ src/Replicant/GlobalUsings.cs
@@ -3,4 +3,6 @@ global using System.Net.Http;
 global using System.Reflection;
 global using System.Text.Json;
 global using System.Diagnostics.CodeAnalysis;
+global using System.Globalization;
 global using System.Net.Http.Headers;
+global using Replicant;
