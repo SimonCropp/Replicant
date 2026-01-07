f3281e23a69249cf034fb99d8e855194dd49813c
Simon Cropp <simon.cropp@gmail.com>
2026-01-07 20:39:00 +1100

    Update TimestampTests.cs

--- src/Tests/TimestampTests.cs
+++ src/Tests/TimestampTests.cs
@@ -19,4 +19,21 @@ public class TimestampTests
 
         return Verify(timestamps);
     }
+
+    [Test]
+    public void FromPath_EtagParsedCorrectly()
+    {
+        // Verify etag is parsed correctly without off-by-one error
+        // Format: {hash}_{date}_{etag}.bin
+        // - hash: 40 chars (indexOf = 40 for first underscore)
+        // - date: 17 chars (yyyy-MM-ddTHHmmss)
+        // - etag: starts at indexOf + 19 (after hash + underscore + date + underscore)
+        var path = "/Dir/92cb8761e3e5246f873da524f438f95468eb3d36_2021-03-22T095023_SmyEtagValue.bin";
+
+        var timestamp = Timestamp.FromPath(path);
+
+        // The etag ForFile should be exactly "SmyEtagValue" (S prefix + value)
+        // If there's an off-by-one error, it would be "myEtagValue" (missing S)
+        AreEqual("SmyEtagValue", timestamp.Etag.ForFile);
+    }
 }
